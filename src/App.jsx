75e7b7be1b1b6e7ffca13ea86c5ab1b75c9f9b7a
lgjohann <guilherme4695@gmail.com>
2023-02-18 01:48:16 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Infos from './components/infos'
+import Infos from './components/Infos'
 import About from './components/About'
 import Interests from './components/Interests'
 import Footer from './components/Footer'
