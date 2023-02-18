1ceb6dd05742fb7e01d243884b4a39dbe8693578
lgjohann <guilherme4695@gmail.com>
2023-02-18 12:02:04 -0300

    third commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const message = ` Currently doing Systems Analysis and Development Technologistat Estácio de Sá, fascinated since childhood by technology, having spent mychildhood around computers.
- Looking forward to deepening my knowledge in Front-end Development using  technologies such as HTML, CSS, Javascript/Typescript, and ReactJS.`;
+const message = ` Currently doing Systems Analysis and Development Technologistat Estácio de Sá, fascinated since childhood by technology, having spent my childhood around computers.
+ Looking forward to deepening my knowledge in Front-end Development using technologies such as HTML, CSS, Javascript/Typescript, and ReactJS.`;
 
 export default function About() {
   return (
